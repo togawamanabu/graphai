8f12ad7ab8b8a98c05a7fad0e73aa2a26be7f951
togawa manabu <togawamanabu@gmail.com>
2024-05-14 22:33:38 +0200

    input file path

--- samples/openai_vision_agent_sample/vision.ts
+++ samples/openai_vision_agent_sample/vision.ts
@@ -9,14 +9,14 @@ const graph_data = {
   version: 0.3,
   nodes: {
     imagePath: {
-      agent: async () => "/Users/manabutogawa/Downloads/addb2f8c-92e7-483f-b747-785085103cfa.jpg", // input({ message: "Path to the image file:" }),
+      agent: async () => input({ message: "Path to the image file:" }),
     },
     vision: {
       agent: "openaiVisionAgentInfo",
       params: {
         model: "gpt-4o",
         verbose: true,
-        query: '画像の状況や映っているものを日本語で説明してください。'
+        query: '画像の状況や映っているものをできるだけ正確かつ丁寧に説明してください。'
       },
       inputs: [undefined, undefined, ":imagePath"],
       isResult: true,
